e37caa7271e289f2532fc1d0a54e3d593d2a48dd
Thomas MIRAULT <thomasmirault@gmail.com>
2022-02-05 17:21:23 +0100

    Révision du cahier des charges, rendu fonctionnel et conforme aux contraintes

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-const touitForm = document.forms[0];
-const submitButton = document.querySelector('#submitButton');
-const touitSection = document.querySelector('#touitSection');
+function addTouit() {
+    const touitForm = document.forms[0];
+    const submitButton = document.querySelector('#submitButton');
+    const touitSection = document.querySelector('#touitSection');
+    const profil = document.querySelector('#pseudo');
+    
+    
+    
+    submitButton.addEventListener('click', function(ev) {
+        ev.preventDefault();
+    
+        const touit = document.createElement("div");
+        touit.classList.add("even");
+    
+        const touitText = document.createElement("p");
+        touitText.classList.add("touitText");
+        touitText.textContent = touitForm["inputTouit"].value;
+    
+        const touitAuthor = document.createElement("div");
+        touitAuthor.classList.add("authorProfile");
+    
+        const touitosLogo = document.createElement("picture");
+        touitosLogo.classList.add("touitosLogo");
+    
+        const author = document.createElement("div");
+        author.classList.add("author")
+        author.textContent = profil.value;
+    
+        const touitosImg = document.createElement("img");
+        touitosImg.src = "images/dog.png";
+        touitosImg.alt = "Profil photo of AstroTouitos";
+    
+        touitAuthor.appendChild(author)
+        touitAuthor.appendChild(touitosLogo)
+        touitosLogo.appendChild(touitosImg);
+        touit.appendChild(touitText);
+        touit.appendChild(touitAuthor);
+        touitSection.appendChild(touit);
+        touitSection.insertBefore(touit, touitSection.firstChild)
+        touitForm.reset();
+    });
+    
+}
 
-touitForm.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-
-    const touit = document.createElement("div");
-    touit.classList.add("even");
-
-    const touitText = document.createElement("p");
-    touitText.classList.add("touitText");
-    touitText.textContent = touitForm["inputTouit"].value;
-
-    const touitosLogo = document.createElement("picture");
-    touitosLogo.classList.add("touitosLogo");
-
-    const touitosImg = document.createElement("img");
-    touitosImg.src = "images/alien.png";
-    touitosImg.alt = "Profil photo of AstroTouitos";
-
-    touitosLogo.appendChild(touitosImg);
-    touit.appendChild(touitText);
-    touit.appendChild(touitosLogo);
-    touitSection.appendChild(touit);
-
-    touitForm.reset();
-});
-
-
-// <div class="even">
-//     <p class="touitText">Petit Touit de l'espace</p>
-//     <picture class="touitosLogo">
-//         <img src="images/dog.png" alt="Profil photo of AstroTouitos">
-//     </picture>
-// </div>
+addTouit();
