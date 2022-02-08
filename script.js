7ab1fe3056cd13420f634aac05412d76d15cbc1b
Thomas MIRAULT <thomasmirault@gmail.com>
2022-02-08 17:11:15 +0100

    Passage mac

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// Déclaration des variables qui serviront pour nourrir le DOM
 const touitForm = document.forms[0];
 const submitButton = document.querySelector("#submitButton");
 const touitSection = document.querySelector("#touitSection");
 const profil = document.querySelector("#pseudo");
+
+// Mise en place de la fonction addTouit qui permet de recevoir les Touits
 
 function addTouit(msg, auth) {
   const touit = document.createElement("div");
@@ -31,27 +34,29 @@ function addTouit(msg, auth) {
   touit.appendChild(touitText);
   touit.appendChild(touitAuthor);
   touitSection.appendChild(touit);
-  touitSection.insertBefore(touit, touitSection.firstChild);
+  touitSection.prepend(touit);
   touitForm.reset();
 }
 
-// const request = new XMLHttpRequest();
-// request.open("GET", "http://touiteur.cefim-formation.org/list", true);
-// request.addEventListener("readystatechange", function () {
-//   if (request.readyState === XMLHttpRequest.DONE) {
-//     const response = JSON.parse(request.responseText);
-//     console.log(response);
-//     const object = response.messages;
+const timeSet = setInterval(function () {
+  const request = new XMLHttpRequest();
+  request.open("GET", "http://touiteur.cefim-formation.org/list", true);
+  request.addEventListener("readystatechange", function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      const response = JSON.parse(request.responseText);
+      console.log(response);
+      const object = response.messages;
 
-//     for (let i = object.length - 100; i < object.length; i++) {
-//       console.log(object[i].message);
-//       let msg = object[i].message;
-//       let auth = object[i].name;
-//       addTouit(msg, auth);
-//     }
-//   }
-// });
-// request.send();
+      for (let i = object.length - 100; i < object.length; i++) {
+        console.log(object[i].message);
+        let msg = object[i].message;
+        let auth = object[i].name;
+        addTouit(msg, auth);
+      }
+    }
+  });
+  request.send();
+}, 1000);
 
 submitButton.addEventListener("click", function () {
   let name;
@@ -59,17 +64,13 @@ submitButton.addEventListener("click", function () {
   touit = touitForm["inputTouit"].value;
   pseudo = profil.value;
 
-  console.log(touit);
-  console.log(pseudo);
   const request2 = new XMLHttpRequest();
-  const params = "message=touit&name=pseudo";
+  const params = "message=" + touit + "&name=" + pseudo;
   request2.open("POST", "http://touiteur.cefim-formation.org/send", true);
   request2.setRequestHeader(
     "Content-type",
     "application/x-www-form-urlencoded"
   );
-  request2.setRequestHeader("Content-lenght", params.length);
-  request2.setRequestHeader("Connection", "close");
   request2.addEventListener("readystatechange", function () {
     if (request2.readyState === XMLHttpRequest.DONE) {
       return;
@@ -82,6 +83,3 @@ submitButton.addEventListener("click", function () {
 
   touitForm.reset();
 });
-
-// envoyer des parametres dans le corps de la requete*
-// et trouver dans quelle langage on va ecrire ses parametre (url encording)
