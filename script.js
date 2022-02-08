bedbae64458e956eb735e73aba53ac2ad34284d4
Thomas MIRAULT <thomasmirault@gmail.com>
2022-02-08 09:28:33 +0100

    Récupération des touits terminé, reste à en envoyer

--- script.js
+++ script.js
@@ -1,45 +1,68 @@
-function addTouit() {
-    const touitForm = document.forms[0];
-    const submitButton = document.querySelector('#submitButton');
-    const touitSection = document.querySelector('#touitSection');
-    const profil = document.querySelector('#pseudo');
-    
-    
-    
-    submitButton.addEventListener('click', function(ev) {
-        ev.preventDefault();
-    
-        const touit = document.createElement("div");
-        touit.classList.add("even");
-    
-        const touitText = document.createElement("p");
-        touitText.classList.add("touitText");
-        touitText.textContent = touitForm["inputTouit"].value;
-    
-        const touitAuthor = document.createElement("div");
-        touitAuthor.classList.add("authorProfile");
-    
-        const touitosLogo = document.createElement("picture");
-        touitosLogo.classList.add("touitosLogo");
-    
-        const author = document.createElement("div");
-        author.classList.add("author")
-        author.textContent = profil.value;
-    
-        const touitosImg = document.createElement("img");
-        touitosImg.src = "images/dog.png";
-        touitosImg.alt = "Profil photo of AstroTouitos";
-    
-        touitAuthor.appendChild(author)
-        touitAuthor.appendChild(touitosLogo)
-        touitosLogo.appendChild(touitosImg);
-        touit.appendChild(touitText);
-        touit.appendChild(touitAuthor);
-        touitSection.appendChild(touit);
-        touitSection.insertBefore(touit, touitSection.firstChild)
-        touitForm.reset();
-    });
-    
+const touitForm = document.forms[0];
+const submitButton = document.querySelector("#submitButton");
+const touitSection = document.querySelector("#touitSection");
+const profil = document.querySelector("#pseudo");
+
+function addTouit(msg, auth) {
+  const touit = document.createElement("div");
+  touit.classList.add("odd");
+
+  const touitText = document.createElement("p");
+  touitText.classList.add("touitText");
+  touitText.textContent = msg;
+
+  const touitAuthor = document.createElement("div");
+  touitAuthor.classList.add("authorProfile");
+
+  const touitosLogo = document.createElement("picture");
+  touitosLogo.classList.add("touitosLogo");
+
+  const author = document.createElement("div");
+  author.classList.add("author");
+  author.textContent = auth;
+
+  const touitosImg = document.createElement("img");
+  touitosImg.src = "images/alien.png";
+  touitosImg.alt = "Profil photo of AstroTouitos";
+
+  touitAuthor.appendChild(author);
+  touitAuthor.appendChild(touitosLogo);
+  touitosLogo.appendChild(touitosImg);
+  touit.appendChild(touitText);
+  touit.appendChild(touitAuthor);
+  touitSection.appendChild(touit);
+  touitSection.insertBefore(touit, touitSection.firstChild);
+  touitForm.reset();
 }
 
-addTouit();
+// const request = new XMLHttpRequest();
+// request.open("GET", "http://touiteur.cefim-formation.org/list", true);
+// request.addEventListener("readystatechange", function () {
+//   if (request.readyState === XMLHttpRequest.DONE) {
+//     const response = JSON.parse(request.responseText);
+//     console.log(response);
+//     const object = response.messages;
+
+//     for (let i = object.length - 100; i < object.length; i++) {
+//       console.log(object[i].message);
+//       let msg = object[i].message;
+//       let auth = object[i].name;
+//       addTouit(msg, auth);
+//     }
+//   }
+// });
+// request.send();
+
+const request2 = new XMLHttpRequest();
+request2.open("POST", "http://touiteur.cefim-formation.org/send", true);
+request2.addEventListener("click", function () {
+  if (request2.readyState === XMLHttpRequest.DONE) {
+    const response = JSON.parse(request2.responseText);
+    console.log(response);
+    const object = response.messages;
+    let msg = touitForm["inputTouit"].value;
+    let auth = profil.value;
+    addTouit(msg, auth);
+  }
+});
+request2.send();
